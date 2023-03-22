9c1a1ff97023893ccf75adec485dd3d5e1feeaec
Aleksa <72038770+AlesyaRabushka@users.noreply.github.com>
2023-03-22 23:19:19 +0300

    added rdflib to work with rdf files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from interface import *
 from dialog import *
+from exit import *
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
@@ -8,26 +9,79 @@ from PyQt5.QtWidgets import QApplication
 class App:
     def __init__(self):
         Form, Window = uic.loadUiType("interface.ui")
-        Form_d, Dialog = uic.loadUiType("dialog.ui")
+        Form_Dialog, Dialog = uic.loadUiType("dialog.ui")
+        Form_Exit, Exit = uic.loadUiType("exit.ui")
+        Form_Edit, Edit = uic.loadUiType("edit.ui")
+
         app = QApplication([])
 
-        window = Window()
-        form = Form()
-        form.setupUi(window)
+        # Main Window
+        self.window = Window()
+        self.form = Form()
+        self.form.setupUi(self.window)
 
-        self.dialog = Dialog()
-        form_d = Form_d()
-        form_d.setupUi(self.dialog)
+        # Dialog Popup
+        self.add_dialog = Dialog()
+        self.form_add_dialog = Form_Dialog()
+        self.form_add_dialog.setupUi(self.add_dialog)
 
-        window.show()
+        # Exit Popup
+        self.exit = Exit()
+        self.form_exit = Form_Exit()
+        self.form_exit.setupUi(self.exit)
 
+        # Edit Popup
+        self.edit = Edit()
+        self.form_edit = Form_Edit()
+        self.form_edit.setupUi(self.edit)
 
+        # for exit dialog to know which dialog to close
+        self.current_dialog_window = ''
 
-        form.pushButton.clicked.connect(self.add)
+        self.window.show()
+
+        # all button clickes configuration
+        self.settings_configuration()
+
         app.exec_()
 
-    def add(self):
-        self.dialog.show()
+
+
+    def settings_configuration(self):
+        # Main Window
+
+        # передача параметров в функцию при нажатии на кнопку???
+        self.form.add_button.clicked.connect(self.add_dialog.show)
+        self.form.edit_button.clicked.connect(self.edit.show)
+
+
+
+        # Dialog Window
+        self.form_add_dialog.add_button.clicked.connect(self.show_dialog_info)
+        self.form_add_dialog.back_button.clicked.connect(self.exit.show)
+        self.form_add_dialog.class_box.addItems(['Mama','Papa']) # add items
+
+        # Exit Window
+        self.form_exit.yes_button.clicked.connect(self.exit.close)
+        if self.current_dialog_window == 'add':
+            self.form_exit.yes_button.clicked.connect(self.add_dialog.close)
+        elif self.current_dialog_window == 'edit':
+            self.form_exit.yes_button.clicked.connect(self.edit.close)
+        self.form_exit.no_button.clicked.connect(self.exit.close)
+
+        # Edit Window
+        # self.form_dialog.add_button.clicked.connect()
+        self.form_edit.back_button.clicked.connect(self.exit.show)
+
+
+    def set_current_add_dialog(self):
+        self.current_dialog_window = 'add'
+    def set_current_edit_dialog(self):
+        self.current_dialog_window = 'edit'
+
+    def show_dialog_info(self):
+        print(self.form_dialog.lineEdit.displayText())
+
 
 if __name__ == '__main__':
     app = App()
