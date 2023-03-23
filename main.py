a81c6cf6d7500b65f44ea5acc8e37b4f18b67eec
Aleksa <72038770+AlesyaRabushka@users.noreply.github.com>
2023-03-23 13:18:03 +0300

    added open file method

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from dialog import *
 from exit import *
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog
+
 
 
 from model import Model
 
 
-class App:
+class App(QWidget):
     def __init__(self):
+        app = QApplication([])
+        super().__init__()
+
         Form, Window = uic.loadUiType("interface.ui")
         Form_Dialog, Dialog = uic.loadUiType("dialog.ui")
         Form_Exit, Exit = uic.loadUiType("exit.ui")
         Form_Edit, Edit = uic.loadUiType("edit.ui")
 
-        app = QApplication([])
+
 
         self.model = Model()
 
@@ -54,35 +58,71 @@ class App:
 
     def settings_configuration(self):
         # Main Window
-
-        # передача параметров в функцию при нажатии на кнопку???
-        self.form.add_button.clicked.connect(self.add_dialog.show)
-        self.form.edit_button.clicked.connect(self.edit.show)
-
+        self.form.open_file_button.clicked.connect(self.open_file)
+        self.form.add_button.clicked.connect(self.add_dialog.open)
+        self.form.edit_button.clicked.connect(self.edit.open)
 
 
-        # Dialog Window
+        # Open File window
+
+
+
+        # Add Dialog Window
         self.form_add_dialog.add_button.clicked.connect(self.show_dialog_info)
-        self.form_add_dialog.back_button.clicked.connect(self.exit.show)
-        self.form_add_dialog.class_box.addItems(self.model.get_classes()) # add items
+        self.form_add_dialog.back_button.clicked.connect(self.add_dialog.close)
+        # self.form_add_dialog.back_button.clicked.connect(self.exit.show)
+        self.form_add_dialog.class_box.addItems(self.model.get_classes()) # to select one of the classes
 
-        # Exit Window
-        self.form_exit.yes_button.clicked.connect(self.exit.close)
-        if self.current_dialog_window == 'add':
-            self.form_exit.yes_button.clicked.connect(self.add_dialog.close)
-        elif self.current_dialog_window == 'edit':
-            self.form_exit.yes_button.clicked.connect(self.edit.close)
-        self.form_exit.no_button.clicked.connect(self.exit.close)
+
 
         # Edit Window
         # self.form_dialog.add_button.clicked.connect()
-        self.form_edit.back_button.clicked.connect(self.exit.show)
+        self.form_edit.back_button.clicked.connect(self.edit.close)
+        # self.form_edit.back_button.clicked.connect(self.exit.show)
+
+
+
+
+
+        # Exit Window
+        self.form_exit.yes_button.clicked.connect(
+            lambda checked, button=self.form_exit.yes_button: self._on_click_back(self.form_exit.yes_button, 'add',
+                                                                                  self.add_dialog))
+        # if self.current_dialog_window == 'add':
+        #     print('here')
+        #     self.form_exit.yes_button.clicked.connect(self.add_dialog.close)
+        # elif self.current_dialog_window == 'edit':
+        #     self.form_exit.yes_button.clicked.connect(self.edit.close)
+        #     print('edit')
+        self.form_exit.no_button.clicked.connect(self.exit.close)
+
+    def open_file(self):
+        # filename, ok = QFileDialog.getOpenFileName(
+        #     self,
+        #     "Select a File",
+        #     "D:\Kyrs3\PBZ_6sem\ontologies-editor\Documents",
+        #     "Text File (*.owl)"
+        # )
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.file, _ = QFileDialog.getOpenFileName(self, "Выбрать файл", "D:\Kyrs3\PBZ_6sem\ontologies-editor\Documents", "Text Files (*.owl)", options=options)
+
+
+
+    def _on_click_back(self, button, window_name, window):
+        print(self.current_dialog_window)
+        if window_name == 'add':
+            button.clicked.connect(self.add_dialog.close)
+            button.clicked.connect(button.close)
+
 
 
     def set_current_add_dialog(self):
         self.current_dialog_window = 'add'
+        print('add')
     def set_current_edit_dialog(self):
         self.current_dialog_window = 'edit'
+        print('edit')
 
     def show_dialog_info(self):
         print(self.form_dialog.lineEdit.displayText())
